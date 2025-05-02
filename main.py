96a1a38bda258d97de0a47d00e5a7b22e62b3ec2
Matheus <moreiracontatosmatheus@hotmail.com>
2025-05-02 18:38:45 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+# Configuração do CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP, incluindo OPTIONS
+    allow_headers=["*"],  # Permite todos os headers
+)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = "https://api.groq.com/openai/v1"
+
+
+class MessageRequest(BaseModel):
+    message: str
+
+@app.post("/chat")
+async def chat_endpoint(data: MessageRequest):
+    user_message = data.message
+    messages = [
+        {"role": "system", "content": "Você é um assistente da FURIA."},
+        {"role": "user", "content": user_message}
+    ]
+    response = openai.ChatCompletion.create(
+        model="compound-beta-mini",
+        messages=messages
+    )
+    bot_message = response['choices'][0]['message']['content']
+    return {"response": bot_message}
+
+
+
